347e95b65b3309a88a7ff429d7d6555c2eb3ae3c
João Tambue <tjoao7279@gmail.com>
2026-08-25 22:34:55 +0100

    Configuracao inicial do ProxyModule

---
+++ src/config/gateway.config.ts
@@ -0,0 +1,18 @@
+export const serverConfig = {
+  users: {
+    url: process.env.USERS_SERVICE_URL || 'http://localhost:3001',
+    timeout: 1000,
+  },
+  products: {
+    url: process.env.PRODUCTS_SERVICE_URL || 'http://localhost:3002',
+    timeout: 1000,
+  },
+  checkout: {
+    url: process.env.CHECKOUT_SERVICE_URL || 'http://localhost:3003',
+    timeout: 1000,
+  },
+  payments: {
+    url: process.env.PAYMENTS_SERVICE_URL || 'http://localhost:3004',
+    timeout: 1000,
+  },
+} as const;
